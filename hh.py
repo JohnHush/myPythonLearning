3e6801b5205ff837589504dd0b641398dceb2c86
JohnHush <luohenggogogo@aliyun.com>
2015-11-22 23:36:12 +0800

    November 22th, listening music from Weiwei Tan& John Lennon

--- hh.py
+++ hh.py
@@ -1,3 +1,5 @@
+import functools
+
 class Student(object):
 
     def __init__( self , name , score ):
@@ -7,8 +9,40 @@ class Student(object):
     def print_score( self ):
         print '%s: %s ' %(self.name, self.score)
 
+    def get_score( self ):
+        return self.score
+def build( x , y ):
+    return lambda : y*y + x*x
 
+def log(text):
+    if callable(text):
+        @functools.wraps(text)
+        def wrapper(*args, **kw):
+            print 'begin call: ' + text.__name__
+            text(*args, **kw)
+            print 'end call: ' + text.__name__
+        return wrapper
+    else:
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kw):
+                print 'begin call: ' + text
+                func(*args, **kw)
+                print 'end call: ' + text
+            return wrapper
+        return decorator
+
+@log
+def  now1():
+    print 'doing1...'
+
+@log('text')
+def now2():
+    print 'doing2...'
 
 if __name__ == "__main__":
-    bart = Student( 'Bart Johnson' , 55 )
-    bart.print_score()
+
+    now1()
+    now2()
+
+
---
+++ test2.py
@@ -0,0 +1,104 @@
+import functools
+
+def bSearch(L, e, low, high):
+    if high - low < 2:
+        return L[low] == e or L[high] == e
+    mid = low + int((high - low)/2)
+    if L[mid] == e:
+        return True
+    if L[mid] > e:
+        return bSearch(L, e, low, mid - 1)
+    else:
+        return bSearch(L, e, mid + 1, high)
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper( *args , **kw ):
+        print "the functiona name is:%s" %func.__name__
+        return func( *args , **kw )
+    return wrapper
+
+@log
+def selSort(L):
+    """Assumes that L is a list of elements that can be compared
+       using >.  Sorts L in ascending order"""
+    for i in range(len(L) - 1):
+        #Invariant: the list L[:i] is sorted
+        minIndx = i
+        minVal= L[i]
+        j = i + 1
+        while j < len(L):
+            if minVal > L[j]:
+                minIndx = j
+                minVal= L[j]
+            j += 1
+        temp = L[i]
+        L[i] = L[minIndx]
+        L[minIndx] = temp
+        print 'Partially sorted list =', L
+selSort([1,3,5,99,33,12])
+print selSort.__name__
+def merge(left, right, lt):
+    """Assumes left and right are sorted lists.
+     lt defines an ordering on the elements of the lists.
+     Returns a new sorted(by lt) list containing the same elements
+     as (left + right) would contain."""
+    result = []
+    i,j = 0, 0
+    while i < len(left) and j < len(right):
+        if lt(left[i], right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while (i < len(left)):
+        result.append(left[i])
+        i += 1
+    while (j < len(right)):
+        result.append(right[j])
+        j += 1
+    return result
+            
+def sort(L, lt = lambda x,y: x < y):
+    """Returns a new sorted list containing the same elements as L"""
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = int(len(L)/2)
+        left = sort(L[:middle], lt)
+        right = sort(L[middle:], lt)
+        print 'About to merge', left, 'and', right
+        return merge(left, right, lt)
+
+##L = [35, 4, 5, 29, 17, 58, 0]
+##newL = sort(L)
+##print 'Sorted list =', newL
+##L = [1.0, 2.25, 24.5, 12.0, 2.0, 23.0, 19.125, 1.0]
+##newL = sort(L, float.__lt__)
+##print 'Sorted list =', newL
+
+def lastNameFirstName(name1, name2):
+    import string
+    name1 = string.split(name1, ' ')
+    name2 = string.split(name2, ' ')
+    if name1[1] != name2[1]:
+        return name1[1] < name2[1]
+    else:
+        return name1[0] < name2[0]
+
+def firstNameLastName(name1, name2):
+    import string
+    name1 = string.split(name1, ' ')
+    name2 = string.split(name2, ' ')
+    if name1[0] != name2[0]:
+        return name1[0] < name2[0]
+    else:
+        return name1[1] < name2[1]
+
+##L = ['John Guttag', 'Tom Brady', 'Chancellor Grimson', 'Gisele Brady',
+##     'Big Julie']
+##newL = sort(L, lastNameFirstName)
+##print 'Sorted list =', newL
+##newL = sort(L, firstNameLastName)
+##print 'Sorted list =', newL
