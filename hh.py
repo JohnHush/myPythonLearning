04c0bed037f971a154a01179c9996a4436546abb
JohnHush <luohenggogogo@aliyun.com>
2015-11-23 09:19:17 +0800

    merge hh.py

--- hh.py
+++ hh.py
@@ -8,7 +8,29 @@ class Student(object):
         print '%s: %s ' %(self.name, self.score)
 
 
+class Animal(object):
+    def run(self):
+        print "Animal is running"
+
+class dog(Animal):
+    def run( self ):
+        print "Dog is runningiiiiiiiiiii"
+
+class cat(Animal):
+    def run( self ):
+        print "cat is runningiiiiiiiiiii so fastttttttttt"
+
+def run_twice(Animal):
+    Animal.run()
+    Animal.run()
+
+def plus( x , y , f ):
+    return f(x) + f(y)
+
 
 if __name__ == "__main__":
-    bart = Student( 'Bart Johnson' , 55 )
-    bart.print_score()
+
+    print plus( 10, -10 , abs )
+
+
+
